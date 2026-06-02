b2c43e7a0f5767db9e1c52d68e4d70e1ad0abd3a
KhuongSpring <boyzsno1@gmail.com>
2026-06-02 08:56:29 +0700

    update: import file

--- src/main/java/com/enterprise/aigateway/common/RestApiV1.java
+++ src/main/java/com/enterprise/aigateway/common/RestApiV1.java
@@ -3,7 +3,11 @@ package com.enterprise.aigateway.common;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
